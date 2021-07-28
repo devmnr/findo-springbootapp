109b6a5d6e5bcdb49288d1665ed8dacfe89cbe03
devmnr <87806903+devmnr@users.noreply.github.com>
2021-07-29 00:24:21 +0530

    Add files via upload

---
+++ src/main/java/com/springboot/fundraising/controller/HomeController.java
@@ -0,0 +1,60 @@
+package com.springboot.fundraising.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.springboot.fundraising.repository.Donor;
+
+@RestController
+public class HomeController {
+	
+//	@RequestMapping("/home")
+//	public String home() {
+//		
+//		return "home";
+//	}
+	
+	@RequestMapping("/")
+	public String home() {
+		
+		return "home";
+	}
+	
+	@GetMapping("/login")
+	public ModelAndView login() {
+	   ModelAndView mv=new ModelAndView("login");
+	   return mv;
+	}
+	
+//	@RequestMapping("/login")
+//	public String login() {
+//		
+//		return "login";
+//	}
+	
+	@RequestMapping("/register")
+	public String register() {
+		
+		return "register";
+	}
+	
+//	@RequestMapping("/error")
+//	public String errorpage() {
+//		
+//		return "error";
+//	}
+	
+	@RequestMapping(path="/donorform", method = RequestMethod.POST)
+	public String donorData(@ModelAttribute("Donor") Donor donor,Model modal) {
+		
+		System.out.println(donor);
+		return "";
+	}
+
+}
